3523f8e1afbf1d4517c0c000fa8c812806a54953
Hong Minhee <minhee@dahlia.kr>
2012-04-06 06:30:56 +0900

    Added a test for greenlet
    --HG--
    branch : transaction

--- sidertests/threadlocal.py
+++ sidertests/threadlocal.py
@@ -1,4 +1,8 @@
 import threading
+try:
+    import greenlet
+except ImportError:
+    greenlet = None
 from attest import Tests, assert_hook, raises
 from sider.threadlocal import get_ident
 
@@ -7,7 +11,7 @@ tests = Tests()
 
 
 @tests.test
-def ident():
+def get_ident_thread():
     result = [None, None]
     def test(value, result_idx):
         a = {get_ident(): value}
@@ -19,4 +23,20 @@ def ident():
     t1.join()
     t2.join()
     assert result == [123, 456]
+
+
+if greenlet:
+    @tests.test
+    def get_ident_greenlet():
+        result = [None, None]
+        def test(value, result_idx, next_):
+            a = {get_ident(): value}
+            next_[0].switch(*next_[1:])
+            result[result_idx] = a.get(get_ident())
+            if next_:
+                next_[0].switch(*next_[1:])
+        g1 = greenlet.greenlet(test)
+        g2 = greenlet.greenlet(test)
+        g1.switch(123, 0, (g2, 456, 1, (g1, 123, 0, (g2, 456, 1, None))))
+        assert result == [123, 456]
 
