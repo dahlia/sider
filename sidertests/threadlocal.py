c86465e210664d16e0baf9e50eb09ff608248f15
Hong Minhee <minhee@dahlia.kr>
2012-04-06 17:17:12 +0900

    Did refactoring for threadlocal test
    --HG--
    branch : transaction

--- sidertests/threadlocal.py
+++ sidertests/threadlocal.py
@@ -10,33 +10,57 @@ from sider.threadlocal import get_ident
 tests = Tests()
 
 
-@tests.test
-def get_ident_thread():
-    result = [None, None]
-    def test(value, result_idx):
-        a = {get_ident(): value}
-        result[result_idx] = a.get(get_ident())
-    t1 = threading.Thread(target=test, args=(123, 0))
-    t2 = threading.Thread(target=test, args=(456, 1))
+def thread_test(generator, args1=(), args2=()):
+    def test(g, *args):
+        g.next()
+        for arg in args:
+            try:
+                g.send(arg)
+            except StopIteration:
+                break
+    t1 = threading.Thread(target=test, args=(generator(),) + args1)
+    t2 = threading.Thread(target=test, args=(generator(),) + args2)
     t1.start()
     t2.start()
     t1.join()
     t2.join()
-    assert result == [123, 456]
 
 
 if greenlet:
-    @tests.test
-    def get_ident_greenlet():
+    def coro_test(generator, args1=(), args2=()):
+        def test(g, (cc, next_args), *args):
+            g.next()
+            for arg in args:
+                try:
+                    g.send(arg)
+                except StopIteration:
+                    break
+                finally:
+                    if cc is not None:
+                        cc.switch(*next_args)
+        c1 = greenlet.greenlet(test)
+        c2 = greenlet.greenlet(test)
+        c1.switch(generator(),
+                  (c2, (generator(), (c1, ())) + args2),
+                  *args1)
+else:
+    def coro_test(*a, **k):
+        pass # skip
+
+
+@tests.test
+def test_get_ident():
+    result = [None, None]
+    local = {}
+    def run():
+        value = yield
+        local[get_ident()] = value
+        result_idx = yield
+        result[result_idx] = local.setdefault(get_ident())
+    thread_test(run, (123, 0), (456, 1))
+    assert result == [123, 456]
+    if greenlet:
         result = [None, None]
-        def test(value, result_idx, next_):
-            a = {get_ident(): value}
-            next_[0].switch(*next_[1:])
-            result[result_idx] = a.get(get_ident())
-            if next_:
-                next_[0].switch(*next_[1:])
-        g1 = greenlet.greenlet(test)
-        g2 = greenlet.greenlet(test)
-        g1.switch(123, 0, (g2, 456, 1, (g1, 123, 0, (g2, 456, 1, None))))
+        coro_test(run, (123, 0), (456, 1))
         assert result == [123, 456]
 
